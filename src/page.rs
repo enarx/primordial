08ce78d2af41be77311fb25dc01843a0414539b3
Nathaniel McCallum <nathaniel@congru.us>
2021-09-06 11:47:09 -0400

    Remove Page::copy()
    The approach in `Pages::copy()` is better.
    
    Signed-off-by: Nathaniel McCallum <nathaniel@congru.us>

--- src/page.rs
+++ src/page.rs
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use core::borrow::{Borrow, BorrowMut};
-use core::mem::{align_of, align_of_val, size_of, size_of_val};
 use core::ops::{Deref, DerefMut};
 
 /// A single page of memory
@@ -76,27 +75,5 @@ impl Page {
     /// Returns a Page full of zeroes
     pub const fn zeroed() -> Self {
         Self([[0; 32]; 16])
-    }
-
-    /// Copy a value into the start of a page
-    ///
-    /// All unused bytes are zero.
-    ///
-    /// # Panics
-    ///
-    /// This function panics if any of these constraints are false:
-    ///   1. `size_of::<Page>() >= size_of_val(&value)`
-    ///   2. `align_of::<Page>() >= align_of_val(&value)`
-    ///   3. `align_of::<Page>() % align_of_val(&value) == 0`
-    pub fn copy<T: Copy>(value: T) -> Page {
-        assert!(size_of::<Page>() >= size_of_val(&value));
-        assert!(align_of::<Page>() >= align_of_val(&value));
-        assert!(align_of::<Page>() % align_of_val(&value) == 0);
-
-        let mut pages = [Page::default()];
-        let bytes = unsafe { pages.align_to_mut::<u8>().1 };
-        let typed = unsafe { bytes.align_to_mut().1 };
-        typed[0] = value;
-        pages[0]
     }
 }
